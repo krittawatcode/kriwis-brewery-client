e122a86404474bdd710efc95582f250b88060337
Krittawat Wisedsuwon <krittawat.w@ngerntidlor.com>
2020-08-07 15:26:59 +0700

    init client

---
+++ src/main/java/kriwis/springframework/kriwisbreweryclient/KriwisBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package kriwis.springframework.kriwisbreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KriwisBreweryClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KriwisBreweryClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kriwis/springframework/kriwisbreweryclient/KriwisBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package kriwis.springframework.kriwisbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KriwisBreweryClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
