e573e75c584d46b26e4d578d4b1af818db999aa9
Krittawat Wisedsuwon <krittawat.code@gmail.com>
2020-08-10 17:12:02 +0700

    add customer client and test

--- src/main/java/kriwis/springframework/kriwisbreweryclient/web/client/BreweryClient.java
+++ src/main/java/kriwis/springframework/kriwisbreweryclient/web/client/BreweryClient.java
@@ -1,6 +1,7 @@
 package kriwis.springframework.kriwisbreweryclient.web.client;
 
 import kriwis.springframework.kriwisbreweryclient.web.model.BeerDto;
+import kriwis.springframework.kriwisbreweryclient.web.model.CustomerDto;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
@@ -14,6 +15,7 @@ import java.util.UUID;
 public class BreweryClient {
 
     public final String BEER_PATH_V1 = "/api/v1/beer/";
+    public final String CUSTOMER_PATH_V1 = "/api/v1/customer/";
 
     private String apihost;
 
@@ -36,10 +38,27 @@ public class BreweryClient {
     }
 
     public void deleteBeer(UUID uuid){
-        restTemplate.delete(apihost + BEER_PATH_V1 + uuid.toString());
+        restTemplate.delete(apihost + BEER_PATH_V1 + uuid);
     }
 
     public void setApihost(String apihost){
         this.apihost = apihost;
     }
+
+    public CustomerDto getCustomerById(UUID uuid){
+        return restTemplate.getForObject(apihost + CUSTOMER_PATH_V1 + uuid.toString(), CustomerDto.class);
+    }
+
+    public URI saveNewCustomer(CustomerDto customerDto){
+        return restTemplate.postForLocation(apihost + CUSTOMER_PATH_V1, customerDto);
+    }
+
+    public void updateCustomer(UUID uuid, CustomerDto customerDto){
+        restTemplate.put(apihost + CUSTOMER_PATH_V1 + "/" + uuid, customerDto);
+    }
+
+    public void deleteCustomer(UUID uuid){
+        restTemplate.delete(apihost + CUSTOMER_PATH_V1 + uuid.toString());
+    }
+
 }
--- src/test/java/kriwis/springframework/kriwisbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/kriwis/springframework/kriwisbreweryclient/web/client/BreweryClientTest.java
@@ -1,6 +1,7 @@
 package kriwis.springframework.kriwisbreweryclient.web.client;
 
 import kriwis.springframework.kriwisbreweryclient.web.model.BeerDto;
+import kriwis.springframework.kriwisbreweryclient.web.model.CustomerDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -40,4 +41,32 @@ class BreweryClientTest {
     void testDeleteBeer(){
         client.deleteBeer(UUID.randomUUID());
     }
+
+    // Customer part
+
+    @Test
+    void testGetCustomerById() {
+        CustomerDto dto = client.getCustomerById(UUID.randomUUID());
+        assertNotNull(dto);
+    }
+
+    @Test
+    void testSavedCustomer(){
+        CustomerDto dto = CustomerDto.builder().name("Prach").build();
+        URI uri = client.saveNewCustomer(dto);
+        assertNotNull(uri);
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void testUpdateCustomer(){
+        CustomerDto dto = CustomerDto.builder().name("New Bear").build();
+        client.updateCustomer(UUID.randomUUID(), dto);
+    }
+
+    @Test
+    void testDeleteCustomer(){
+        client.deleteCustomer(UUID.randomUUID());
+    }
+
 }
